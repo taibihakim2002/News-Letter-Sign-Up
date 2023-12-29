4d323be7f708a95209261a95ffe4eb8194dd407b
taibi abdelhakim <taibihakim2002@gmail.com>
2023-12-29 20:12:59 +0100

    Main Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# News-Letter-Sign-Up
-News Letter Sign Up With Success Message | Frontend Mentor
+# Frontend Mentor - Newsletter sign-up form with success message
+
+![Design preview for the Newsletter sign-up form with success message coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
---
+++ js.js
@@ -0,0 +1,26 @@
+let boxEmailField = document.querySelector(".box .email-field");
+let boxSubmit = document.querySelector(".box .submit-email");
+let thanksDismiss = document.querySelector(".thanks .submit-dismiss");
+let box = document.querySelector(".box")
+let thanks = document.querySelector(".thanks");
+let validation = document.querySelector(".validation");
+let emailForm = document.querySelector(".email-form");
+let thanksEmail = document.querySelector(".thanks-email");
+
+var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+emailForm.onsubmit = function (e) {
+  e.preventDefault();
+  let emailValue = boxEmailField.value;
+  if(emailRegex.test(emailValue)){
+    box.style.display= "none";
+    thanks.style.display = "flex";
+    thanksEmail.innerHTML = emailValue;
+  }else {
+    validation.style.display = "flex";
+    boxEmailField.classList.add("wrong-field")
+  }
+};
+thanksDismiss.onclick = function(){
+  window.location.reload();
+  
+}
